717741667ff0628a606bbbaa312d71206a18f8ce
Patrick Dantas <patrickdsilva99@gmail.com>
2021-03-31 18:08:57 -0300

    comentários adicionados, e lista para armazenar todos os tokens também

--- compilador.py
+++ compilador.py
@@ -151,56 +151,67 @@ def ler_arquivo_mgol(lex):
     lex.qtd_linhas = len(open("mgol.alg").readlines())
     lex.codigo_mgol = f.read() + '\0'
 
-def analisador_lexico(lex):    
-    ler_arquivo_mgol(lex)
+def analisador_lexico(lex): # o analisador léxico é um laço que verifica o código de entrada,
+    ler_arquivo_mgol(lex)   # e armazena cada token, um de cada vez
     print(lex.codigo_mgol)
+    i = 0
+    lista_tokens = []
     while(1):
-        tok = proximo_token(lex)
-        print(tok)
-        if(tok.token == tokens.EndOfFile or tok.erro != ""):
-            break
+        lista_tokens.append(proximo_token(lex))
+        print(lista_tokens[i])
+        if(lista_tokens[i].token == tokens.EndOfFile or lista_tokens[i].erro != ""):
+            break # se o token encontrado for Fim de Arquivo ou houver erro, 
+                  # o laço de repetição que busca o próximo token vai parar
+        i += 1
 
-def tokenizar(c,estado_atual):
+def tokenizar(c,estado_atual): # essa função generaliza todas as entradas pra facilitar minha vida
     if (estado_atual == 2 or estado_atual == 4):
         if (c == 'e'):
             return c
         if (c == 'E'):
             return c
     if (estado_atual == 18):
-        if (c != '"'):
+        if (c != '"'): # enquanto o estado for 18, tudo estará dentro do literal, até que feche as aspas
             return 'A'
         else:
             return c
     if (estado_atual == 16):
-        if (c != '}'):
+        if (c != '}'): # enquanto o estado for 16, tudo estará dentro do comentário, até que feche a chave
             return 'A'
         else:
             return c
     if (c == '\n' or c == ' ' or c == '\t' or c == '\0'):
         return 'S'
-    if (c.isalpha()): 
+    if (c.isalpha()): # se o caractere for alfabético, retorna L
         return 'L'
-    if (c.isdigit()):
+    if (c.isdigit()): # se o caractere for numérico, retorna D
         return 'D'
     else:
         return c
 
 def proximo_token(lex):
-    tk = Token()
-    estado_atual = -1
+    tk = Token()    
+    estado_atual = -1 # o estado inicial é -1
     estado_novo = -1
-    ini_lexema = 0
-    fim_lexema = -1
-    for c in lex.codigo_mgol:
+    ini_lexema = 0    # ini_lexema e fim_lexema marcam as posições de início e o fim de cada lexema
+    fim_lexema = -1   # para retirar cada lexema da string que armazena o código completo
+    for c in lex.codigo_mgol: # vou iterar caractere por caractere no código, até encontrar o próximo token
         if c == '\0' and estado_atual == -1 and estado_novo == -1:
-            tk.linha  = lex.linha
-            tk.lexema = tokens.EndOfFile
-            tk.token  = tokens.EndOfFile
+            tk.linha  = lex.linha         
+            tk.lexema = tokens.EndOfFile 
+            tk.token  = tokens.EndOfFile 
             return tk
         fim_lexema +=1
         t = tokenizar(c,estado_atual)
+
+        # a matriz de estados léxica é um dicionário. A chave é um par de (char,int), e o valor é um int
+        # Chave = (entrada,estado_atual)
+        # Valor = estado_novo
         estado_novo = matriz_de_estados_lexica.get((t,estado_atual),0)
-        if estado_novo == -1 and estado_atual == 20:
+        # em python, utilizando a função get(), posso passar a chave como parâmetro para obter o valor correspondente
+        # se o par (entrada,estado_atual) não for encontrado, retorno o estado 0, que é o estado de erro
+
+        if estado_novo == -1 and estado_atual == 20: # COMENTÁRIO
             print("Linha ",lex.linha," [comentario] ",lex.codigo_mgol[ini_lexema:fim_lexema],"\n")
             ini_lexema = fim_lexema               
             if c == '\n' or c == '\t' or c == ' ':
@@ -208,18 +219,18 @@ def proximo_token(lex):
                 if c == '\n':
                     lex.linha += 1
                     lex.coluna = 1
-        elif estado_novo == -1 and estado_atual != -1:
+        elif estado_novo == -1 and estado_atual != -1: # LEXEMA AINDA NÃO ACABOU
             break
-        elif estado_novo == -1 and estado_atual == -1:
+        elif estado_novo == -1 and estado_atual == -1: # LEXEMA ACABOU
             ini_lexema += 1
             if c == '\n':
                 lex.linha += 1
                 lex.coluna = 1
             if c == '\t' or c == ' ':
                 lex.coluna += 1           
-        elif estado_novo == 0:
-            token = matriz_de_estados_finais.get(estado_atual,None)
-            error = matriz_de_estados_finais.get(matriz_de_estados_lexica.get((t,-1),0),None)
+        elif estado_novo == 0: # TRATAMENTO DE ERRO
+            token = matriz_de_estados_finais.get(estado_atual,None) # Verifica se o estado atual é final
+            error = matriz_de_estados_finais.get(matriz_de_estados_lexica.get((t,-1),0),None) # vefifica se o erro é conhecido
             if token is not None and error is not None:
                 if token != error:
                     if (token == tokens.id_ or token == tokens.num):
@@ -267,17 +278,17 @@ def proximo_token(lex):
             lex.coluna += 1
         estado_atual = estado_novo
 
-    _token = matriz_de_estados_finais[estado_atual]
-    _lexema = lex.codigo_mgol[ini_lexema:fim_lexema]
+    _token = matriz_de_estados_finais.get(estado_atual,None)  # verifico se o estado atual é final
+    _lexema = lex.codigo_mgol[ini_lexema:fim_lexema] # pego o lexema do código
 
-    if (_token):
-        lex.codigo_mgol = lex.codigo_mgol[fim_lexema:len(lex.codigo_mgol)]
+    if (_token is not None):
+        lex.codigo_mgol = lex.codigo_mgol[fim_lexema:len(lex.codigo_mgol)] # atualizo o código deletando o que já foi lido
         if (_token == tokens.id_):
             if (not palavra_reservada(_lexema)):
                 tk.tipo = ""
             else:
-                _token = _lexema                
-                if (_token == tokens.inteiro):
+                _token = _lexema               # se o lexema for uma palavra reservada, pode ser que ele seja um tipo, e
+                if (_token == tokens.inteiro): # os tipos definidos pela linguagem são int, literal e double
                     tk.tipo = "int"
                 elif (_token == tokens.lit):
                     tk.tipo = "literal"
@@ -293,9 +304,9 @@ def proximo_token(lex):
         tk.lexema  = _lexema
         tk.token   = _token
         return tk
-    else:
-        if (estado_atual == 16 or estado_atual == 18):
-            tk.linha = lex.linha
+    else: 
+        if (estado_atual == 16 or estado_atual == 18): # se chegou aqui, então o usuário esqueceu de fechar as chaves ou aspas, 
+            tk.linha = lex.linha                       # pq o estado de retorno sempre tem que ser final
             tk.coluna = lex.coluna
             tk.erro = "Vc esqueceu de fechar alguma coisa aqui: " + _lexema[0:7] + "..."
             return tk
@@ -303,6 +314,8 @@ def proximo_token(lex):
         tk.coluna = lex.coluna
         tk.lexema = _lexema
         return tk
+
+# isso aqui embaixo é a Main
 
 lex = Lex()
 tokens = Lista_de_tokens()
